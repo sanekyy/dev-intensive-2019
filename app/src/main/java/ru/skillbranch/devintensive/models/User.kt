f8cb980262c1d22928247482054cd38b82741a00
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2019-06-27 17:28:23 +0300

    init commit

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,27 @@
+package ru.skillbranch.devintensive.models
+
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+class User(
+    val id: String,
+    var firstName: String?,
+    var lastName: String?,
+    var avatar: String? = null,
+    var rating: Int = 0,
+    var respect: Int = 0,
+    var lastVisit: Date = Date(),
+    var isOnline: Boolean = false
+) {
+
+    companion object Factory {
+        private var lastId: Int = -1
+        fun makeUser(fullName: String?): User {
+            lastId++
+
+            val (firstName, lastName) = Utils.parseFullName(fullName)
+
+            return User(id = "$lastId", firstName = firstName, lastName = lastName)
+        }
+    }
+}
